5179905b2c04a361bf5866b2a789a0a7c0dde8c6
Keith <keithmillerweb@gmail.com>
2014-02-12 04:20:37 +0000

    Validates presence of title, author, and summary for books. Validates ISBN length.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ActiveRecord::Base
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :summary, presence: true
+  validates :ISBN, length: {is: 10}
+  
 end
